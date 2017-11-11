b65143a6466e6f476c7fe161c81dc6e6d12314c5
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2017-11-11 09:55:46 +0530

    removed print statements

--- settings.py
+++ settings.py
@@ -11,10 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import pdb;pdb.set_trace()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print BASE_DIR
 
 
 # Quick-start development settings - unsuitable for production
@@ -68,8 +66,6 @@ TEMPLATES = [
         },
     },
 ]
-
-print TEMPLATES['DIRS'], 'template DRIs'
 
 WSGI_APPLICATION = 'wsgi.application'
 
